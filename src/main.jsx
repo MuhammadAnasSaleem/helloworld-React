8ec568af29d865d4c09462528b5c6b1534f58402
MuhammadAnasSaleem <anassaleem555590@gmail.com>
2024-10-24 00:07:01 +0500

    updated assingment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
+const url =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6c9a34lnjn0vYWJrZ6EV0WSYof_tAYxVVTg&s";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 function App() {
-  return <h1>React app by Muhammad Anas</h1>;
+  return (
+    <>
+      <h1 style={{ fontFamily: "monospace" }}>Famous Sports In The World</h1>
+      <img src={url} alt="football" className="image" width={200} />
+      <ul>
+        <li>Circket</li>
+        <li>Hockey</li>
+        <li>BaseBall</li>
+        <li>BasketBall</li>
+        <li>Football</li>
+      </ul>
+    </>
+  );
 }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Made By Muhammad Anas</h1>
+    <App />
   </StrictMode>
 );
