6145c0eb360c7ee86cb8c4f6be15967a181702f3
MuhammadAnasSaleem <anassaleem555590@gmail.com>
2024-11-04 21:52:20 +0500

    third

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,13 @@ const url =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6c9a34lnjn0vYWJrZ6EV0WSYof_tAYxVVTg&s";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { useState } from "react";
 function App() {
-  return (
-    <>
-      <h1 style={{ fontFamily: "monospace" }}>Famous Sports In The World</h1>
-      <img src={url} alt="football" className="image" width={200} />
-      <ul>
-        <li>Circket</li>
-        <li>Hockey</li>
-        <li>BaseBall</li>
-        <li>BasketBall</li>
-        <li>Football</li>
-      </ul>
-    </>
-  );
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return <button onClick={handleClick}>you clicked {count} time</button>;
 }
 
 createRoot(document.getElementById("root")).render(
